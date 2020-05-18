6407d0e447340f6e01d81eeec89d79c7ed11860f
Akanksha Singh <64551715+AkankshaSingh2215@users.noreply.github.com>
2020-05-18 22:59:51 +0530

    Fun with Turtle

---
+++ turle_game.py
@@ -0,0 +1,18 @@
+import turtle
+my_turtle= turtle.Turtle()
+
+
+def square(n,a):
+    
+ 
+      
+ 
+    my_turtle.forward(n)
+    my_turtle.left(a)
+    my_turtle.forward(n)
+for i in range(20):    
+    square(70,70)
+  
+ 
+ 
+
